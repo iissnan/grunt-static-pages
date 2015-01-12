4c00cbaa5492a17e6b501ceecda892c2d2e0e6d7
iissnan <iissnan@gmail.com>
2015-01-12 17:41:01 +0800

    LESS support

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,20 +4,31 @@ module.exports = function (grunt) {
 
     grunt.config.init({
         settings: {
-            // 设定服务器端口号，例如： 9000
-            port: 9000,
+            server: {
+                // 设定服务器端口号，例如： 9000
+                port: 9000,
 
-            // 设定需要监视的文件目录，默认是与Gruntfile.js同级的目录
-            base: '.',
+                // 设定需要监视的文件目录，默认是与Gruntfile.js同级的目录
+                base: '.',
 
-            // 需要监视的文件类型，例如：.html, .css
-            files: [
-                '**/*.html',
-                '**/*.js',
-                '**/*.css'
-            ]
+                // 设定服务器端口监听的地址，默认是*。例如： 0.0.0.0
+                host: '*'
+            },
+
+            watch: {
+                // 需要监视的文件类型，例如：.html, .css
+                static: ['**/*.html', '**/*.js', '**/*.css']
+            },
+
+            less: {
+                // 设定LESS文件组，形式：{ 目标文件的路径/名字:  LESS文件的路径/名字 }，例如：
+                // "example/css/main.css": "example/less/main.less",
+                // "example/css/extra.css": "example/less/extra.less"
+            }
         }
     });
+
+    var tasks = ['connect', 'watch'];
 
     grunt.config.set('connect', {
         options: {
@@ -25,9 +36,9 @@ module.exports = function (grunt) {
         },
         server: {
             options: {
-                port: '<%= settings.port %>',
-                base: '<%= settings.base %>',
-                hostname: '*'
+                port: '<%= settings.server.port %>',
+                base: '<%= settings.server.base %>',
+                hostname: '<%= settings.server.host %>'
             }
         }
     });
@@ -37,9 +48,44 @@ module.exports = function (grunt) {
             options: {
                 livereload: '<%= connect.options.livereload %>'
             },
-            files: '<%= settings.files %>'
+            files: '<%= settings.watch.static %>'
         }
     });
 
-    grunt.task.registerTask('default', ['connect', 'watch']);
+    // LESS support
+    (function () {
+        var lessSettings = grunt.config.get('settings.less');
+
+        if ( !isEmpty(lessSettings) ) {
+            grunt.config.set('less', {
+                development: {
+                    files: lessSettings
+                }
+            });
+
+            grunt.config.set('watch.less', {
+                files: ['**/*.less'],
+                tasks: ['less']
+            });
+
+            tasks.unshift('less');
+        }
+    }());
+
+    grunt.task.registerTask('default', tasks);
+
+
+    /**
+     * Test if obj is empty.
+     *
+     * @param {Object} obj
+     * @returns {boolean}
+     */
+    function isEmpty(obj) {
+        if (obj == null) return true;
+        for (var p in obj) {
+            if (obj.hasOwnProperty(p)) return false;
+        }
+        return true;
+    }
 };
